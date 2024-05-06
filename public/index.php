4560dc27be845905501fe1cecee4ac928c03b4a6
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2024-05-06 10:12:39 +0200

    refactorisation de index.php

--- public/index.php
+++ public/index.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 require_once '../vendor/autoload.php';
 
 use Database\MyPdo;
+use Html\WebPage;
 
 MyPDO::setConfiguration('mysql:host=mysql;dbname=cutron01_music;charset=utf8', 'web', 'web');
+
+$webPage = new WebPage();
 
 $stmt = MyPDO::getInstance()->prepare(
     <<<'SQL'
@@ -19,5 +22,5 @@ SQL
 $stmt->execute();
 
 while (($ligne = $stmt->fetch()) !== false) {
-    echo "<p>{$ligne['name']}\n</p>";
+    echo "<div>{$ligne['name']}</div>";
 }
