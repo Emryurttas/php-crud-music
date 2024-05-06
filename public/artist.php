f7d45ff0e9fe1516aa75a69fa8dd198bd75510a0
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2024-05-06 16:27:20 +0200

    fin séance

--- public/artist.php
+++ public/artist.php
@@ -2,20 +2,21 @@
 
 declare(strict_types=1);
 
-require_once '../vendor/autoload.php';
-
 use Database\MyPdo;
 use Html\WebPage;
+require_once '../vendor/autoload.php';
 
 MyPDO::setConfiguration('mysql:host=mysql;dbname=cutron01_music;charset=utf8', 'web', 'web');
 
+
 $webPage = new WebPage();
+$artistId = 17;
 
 $stmt = MyPDO::getInstance()->prepare(
     <<<'SQL'
-    SELECT id, name
+    SELECT name
     FROM artist
-    ORDER BY name
+    WHERE id = 
 SQL
 );
 
@@ -25,5 +26,7 @@ while (($line = $stmt->fetch()) !== false) {
     $escape = $webPage->escapeString($line['name']);
     $webPage->appendContent("<div>$escape</div>\n");
 }
+
+
 
 echo $webPage->toHTML();
