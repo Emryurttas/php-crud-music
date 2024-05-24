800c9a28391dd6526f6fbf6f919a19de4820156f
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2024-05-24 13:59:06 +0200

    Creation de la classe Album avec ses attributs et création de la classe AlbumCollection

---
+++ src/Entity/Album.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+class Album
+{
+    private int $id;
+    private string $name;
+    private int $year;
+    private int $artistId;
+    private int $genreId;
+    private int $coverId;
+
+    /**
+     * @return int id
+     */
+    public  function getId(): int
+    {
+        return $this->id;
+    }
+    /**
+     * @return string name
+     */
+    public  function getName(): string
+    {
+        return $this->name;
+    }
+    /**
+     * @return int year
+     */
+    public  function getYear(): int
+    {
+        return $this->year;
+    }
+    /**
+     * @return int artistId
+     */
+    public  function getArtistId(): int
+    {
+        return $this->artistId;
+    }
+    /**
+     * @return int GenreId
+     */
+    public  function getGenreId(): int
+    {
+        return $this->genreId;
+    }
+    /**
+     * @return int coverId
+     */
+    public  function getCoverId(): int
+    {
+        return $this->coverId;
+    }
+}
