f8b24b84f9639d6e9b8f90b2520f0f16eed04f1b
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2024-05-24 15:47:30 +0200

    Amélioration de l'entité « Artist »et creation de la classe EntityNotFoundExcception

--- src/Entity/Artist.php
+++ src/Entity/Artist.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use Entity\Collection\AlbumCollection;
+use PDO;
+
 class Artist
 {
     private int $id;
@@ -21,6 +25,28 @@ class Artist
     public function getName(): string
     {
         return $this->name;
+    }
+
+    public static function findByArtistId(int $id): Artist
+    {
+        $stmtArtistId = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id, name
+            FROM artist
+            WHERE id = :id
+            SQL
+        );
+        $stmtArtistId->execute(['id' => $id]);
+        $artistData = $stmtArtistId->fetch(PDO::FETCH_ASSOC);
+
+        if (!$artistData) {
+            throw new EntityNotFoundException("Artist $id pas trouvé");
+        }
+        return new Artist($artistData['id'], $artistData['name']);
+    }
+    public function getAlbums(): array
+    {
+        return AlbumCollection::findByArtistId($this->id);
     }
 
 }
