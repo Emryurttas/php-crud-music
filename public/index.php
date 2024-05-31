c0fcbbc20ce4896d5aa97d63175fa67092416e11
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2024-05-31 16:25:06 +0200

    correction browse.yml, ajout du script composer,et Tests de la liste des albums d'un artiste test non ok

--- public/index.php
+++ public/index.php
@@ -7,20 +7,16 @@ use Html\AppWebPage;
 
 $webPage = new AppWebPage();
 
-$webPage->setTitle("Mes Musiques");
+$webPage->setTitle("Artistes");
 
-$webPage->appendContent('<header><h1>' . $webPage->getTitle() . '</h1></header>');
-
+$webPage->appendContent('<header class="header"><h1>' . $webPage->escapeString($webPage->getTitle()) . '</h1></header>');
 $webPage->appendContent('<main class="content"><ul class="list">');
 
 $artists = ArtistCollection::findAll();
 foreach ($artists as $artist) {
     $artistId = $artist->getId();
     $artistName = $webPage->escapeString($artist->getName());
-    $webPage->appendContent("<div class='artist'>
-                                        <a class='artist_item' href='artist.php?artistId=$artistId'>
-                                        $artistName</a>
-                                    </div>");
+    $webPage->appendContent("<li class='artist'><a class='artist_item' href='artist.php?artistId=$artistId'>$artistName</a></li>");
 }
 $webPage->appendContent('</ul></main>');
 
