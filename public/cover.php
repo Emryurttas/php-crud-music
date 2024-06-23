97a93f8dbf19e0273efb6a63b364a0be739fd0c4
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2024-06-23 23:05:59 +0200

    gestion des erreurs

--- public/cover.php
+++ public/cover.php
@@ -5,22 +5,32 @@ declare(strict_types=1);
 use Entity\Cover;
 use Entity\Exception\EntityNotFoundException;
 
-
-$id = $_GET['id'];
+$id = $_GET['coverId'];
 
 try {
-    $poster = cover::findById((int)$id);
+    if (empty($id) || !is_numeric($id)) {
+        throw new InvalidArgumentException('Invalid cover ID');
+    }
+
+    $poster = Cover::findById((int)$id);
     $image = $poster->getJpeg();
 
     if (empty($image)) {
-        throw new EntityNotFoundException('image unavailable ');
+        throw new EntityNotFoundException('Image not found');
     }
-} catch (EntityNotFoundException) {
+
+    header("Content-Type: image/jpeg");
+    echo $image;
+
+} catch (InvalidArgumentException) {
+    http_response_code(400);
     exit;
-} catch(Exception){
+
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+    exit;
+
+} catch (Exception $e) {
     http_response_code(500);
     exit;
 }
-
-header("Content-Type: image/jpeg");
-echo $image;
