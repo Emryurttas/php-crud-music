9f35b5c3e4e7b56134affe10f4d5cb0b01ec892a
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2024-06-23 23:05:29 +0200

    suppression element inutiles

--- public/index.php
+++ public/index.php
@@ -5,26 +5,26 @@ declare(strict_types=1);
 use Entity\Collection\ArtistCollection;
 use Html\AppWebPage;
 
-
 try {
     $webPage = new AppWebPage();
-
     $webPage->appendCssUrl('/css/style.css');
 
     $webPage->setTitle("Artistes");
-    $webPage->appendContent('<main class="content"><ul class="list">');
+
+    $webPage->appendContent('<ul class="list">');
 
     $artists = ArtistCollection::findAll();
-    if (!$artists) {
-        throw new Exception('No artists found');
-    }
+
     foreach ($artists as $artist) {
         $artistId = $artist->getId();
         $artistName = $webPage->escapeString($artist->getName());
         $webPage->appendContent("<li class='artist'><a class='artist_item' href='artist.php?artistId=$artistId'>$artistName</a></li>");
     }
-    $webPage->appendContent('</ul></main>');
+
+    $webPage->appendContent('</ul>');
+
     echo $webPage->toHTML();
+
 } catch (Exception $e) {
     http_response_code(500);
     echo "An error occurred: " . $e->getMessage();
