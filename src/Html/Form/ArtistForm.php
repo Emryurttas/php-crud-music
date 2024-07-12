030849324e6f864b73b2f85db3ff0d69ac6f4ad4
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2024-07-12 22:51:26 +0200

    Construire un « Artist » à partir de la « query string » tests ok

--- src/Html/Form/ArtistForm.php
+++ src/Html/Form/ArtistForm.php
@@ -3,7 +3,9 @@
 namespace Html\Form;
 
 use Entity\Artist;
+use Entity\Exception\ParameterException;
 use Html\StringEscaper;
+use InvalidArgumentException;
 
 class ArtistForm
 {
@@ -49,4 +51,36 @@ class ArtistForm
         </form>
     HTML;
     }
+    /**
+     * Définit l'entité à partir des paramètres de la chaîne de requête.
+     *
+     * Cette méthode extrait les données envoyées via la méthode POST pour créer
+     * une instance de l'artiste. Elle effectue les vérifications suivantes :
+     *
+     * 1. Vérifie si un identifiant d'artiste est fourni et s'il est valide.
+     * 2. Vérifie si le nom de l'artiste est présent et non vide.
+     * 3. Supprime les balises HTML et les espaces autour du nom de l'artiste.
+     * 4. Lance une exception si le nom de l'artiste est requis mais manquant ou vide.
+     *
+     * @throws ParameterException Si le nom de l'artiste est requis mais non fourni.
+     */
+    public function setEntityFromQueryString(): void
+    {
+        $artistId = null;
+
+        if (!empty($_POST['id']) && ctype_digit($_POST['id'])) {
+            $artistId = (int)$_POST['id'];
+        }
+        if (empty($_POST['name'])) {
+            throw new ParameterException("Le nom de l'artiste est requis.");
+        }
+
+        $artistName = $this->stripTagsAndTrim($_POST['name']);
+
+        if (empty($artistName)) {
+            throw new ParameterException("Le nom de l'artiste est requis.");
+        }
+        $this->artist = Artist::create($artistName, $artistId);
+    }
+
 }
--- tests/Form/ArtistFormCest.php
+++ tests/Form/ArtistFormCest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Form;
 
+use Codeception\Example;
 use Codeception\Stub;
 use Entity\Artist;
+use Entity\Exception\ParameterException;
 use Exception;
 use Html\Form\ArtistForm;
 use Tests\FormTester;
@@ -44,11 +46,63 @@ class ArtistFormCest
         $I->seeElement('form input[type="text"  ][name="name"][value="Artiste"][required]');
     }
 
+    /**
+     * @throws Exception
+     */
     public function escapeArtistName(FormTester $I): void
     {
         $artist = Stub::make(Artist::class, ['id' => 90, 'name' => 'Art&iste']);
         $I->amTestingPartialHtmlContent((new ArtistForm($artist))->getHtmlForm('go.php'));
 
         $I->seeElement('input[value="Art&iste"]');
+    }
+
+    /**
+     * @dataProvider artistNameProvider
+     * @throws ParameterException
+     */
+    public function getNewArtistDataFromQueryString(FormTester $I, Example $example): void
+    {
+        $_POST['id'] = '';
+        $_POST['name'] = $example['name'];
+        $form = new ArtistForm();
+        $form->setEntityFromQueryString();
+        $I->assertInstanceOf(Artist::class, $form->getArtist());
+        $I->assertNull($form->getArtist()->getId());
+        $I->assertSame($example['expectedName'], $form->getArtist()->getName());
+    }
+
+    /**
+     * @dataProvider artistNameProvider
+     * @throws ParameterException
+     */
+    public function getExistingArtistDataFromQueryString(FormTester $I, Example $example): void
+    {
+        $_POST['id'] = '90';
+        $_POST['name'] = $example['name'];
+        $form = new ArtistForm();
+        $form->setEntityFromQueryString();
+        $I->assertInstanceOf(Artist::class, $form->getArtist());
+        $I->assertSame(90, $form->getArtist()->getId());
+        $I->assertSame($example['expectedName'], $form->getArtist()->getName());
+    }
+
+    protected function artistNameProvider(): array
+    {
+        return [
+            ['name' => 'Artist', 'expectedName' => 'Artist'],
+            ['name' => '    Artist        ', 'expectedName' => 'Artist'],
+            ['name' => 'Artist', 'expectedName' => 'Artist'],
+            ['name' => '      Artist      ', 'expectedName' => 'Artist'],
+        ];
+    }
+
+    public function missingArtistNameFromQueryStringThrowsException(FormTester $I): void
+    {
+        $I->expectThrowable(ParameterException::class, function () {
+            $_POST['id'] = '';
+            $_POST['name'] = '';
+            (new ArtistForm())->setEntityFromQueryString();
+        });
     }
 }
