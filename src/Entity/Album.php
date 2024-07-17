f4b9a0409fa2edcb85555bbcf5993f1573777010
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2024-07-17 22:48:03 +0200

    ajouts methode dans album

--- src/Entity/Album.php
+++ src/Entity/Album.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use Entity\Collection\SongCollection;
+use Entity\Exception\EntityNotFoundException;
+use PDO;
+
 class Album
 {
     private int $id;
@@ -59,5 +64,52 @@ class Album
     public function getCoverId(): int
     {
         return $this->coverId;
+    }
+    public function getArtist(): Artist
+    {
+        return Artist::findById($this->artistId);
+    }
+
+    public function getGenre(): Genre
+    {
+        return Genre::findById($this->genreId);
+    }
+
+    /**
+     * Récupère la liste des morceaux associés à cet album.
+     *
+     * @return array La liste des morceaux de cet album.
+     */
+    public function getSongs(): array
+    {
+        return SongCollection::findByAlbumId($this->id);
+    }
+
+    public static function findById(int $id): Album
+    {
+        $pdo = MyPDO::getInstance();
+        $stmt = $pdo->prepare(
+            <<<SQL
+            SELECT id, name, year, artistId, genreId, coverId
+            FROM album
+            WHERE id = :id
+            SQL
+        );
+        $stmt->execute(['id' => $id]);
+        $albumData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$albumData) {
+            throw new EntityNotFoundException("Album $id non trouvé");
+        }
+
+        $album = new Album();
+        $album->id = (int)$albumData['id'];
+        $album->name = $albumData['name'];
+        $album->year = (int)$albumData['year'];
+        $album->artistId = (int)$albumData['artistId'];
+        $album->genreId = (int)$albumData['genreId'];
+        $album->coverId = (int)$albumData['coverId'];
+
+        return $album;
     }
 }
